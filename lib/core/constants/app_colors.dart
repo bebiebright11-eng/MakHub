7bc200773a87f23f806980616d0140ae006dea97
bebiebright11-eng <bebiebright11@gmail.com>
2026-07-09 22:15:51 +0300

    created the admin dashboard

--- lib/core/constants/app_colors.dart
+++ lib/core/constants/app_colors.dart
@@ -4,17 +4,19 @@ class AppColors {
   AppColors._();
 
   static const Color primary = Color(0xFF1E88E5);
-  static const Color secondary = Color(0xFF42A5F5);
+  static const Color secondary = Color(0xFF64B5F6);
 
   static const Color background = Color(0xFFF8F9FA);
 
   static const Color white = Colors.white;
+  static const Color black = Color(0xFF212121);
 
-  static const Color black = Colors.black87;
+  static const Color grey = Color(0xFF9E9E9E);
+  static const Color lightGrey = Color(0xFFE0E0E0);
 
-  static const Color grey = Colors.grey;
+  static const Color success = Color(0xFF4CAF50);
+  static const Color warning = Color(0xFFFFC107);
+  static const Color error = Color(0xFFE53935);
 
-  static const Color success = Colors.green;
-
-  static const Color error = Colors.red;
+  static const Color cardBackground = Colors.white;
 }
